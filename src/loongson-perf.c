0f5be70b90aa64e99a7a844abd4858f0e7414dc9
zhuyaliang <15132211195@163.com>
2022-07-18 22:43:41 +0800

    Update loongson perf item

--- src/loongson-perf.c
+++ src/loongson-perf.c
@@ -25,7 +25,13 @@ struct _LoongsonPerfPrivate
 {
     char *name;
     char *current_cpu_hz;
+    char *max_cpu_hz;
     char *bogomips;
+    char *physical_kernel;
+    char *max_mem_fre;
+    char *memory_channel;
+    char *max_mem_capacity;
+    char *cpu_threads;
 };
 
 G_DEFINE_TYPE_WITH_PRIVATE (LoongsonPerf, loongson_perf, GTK_TYPE_BOX)
@@ -53,11 +59,95 @@ static void get_cpu_perf_data (LoongsonPerf *perf)
     fclose(cpuinfo);
 }
 
+static void get_cpu_max_cpu_hz (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->max_cpu_hz = loongson_dbus_call ("MaximumCpuFrequency", &error);
+    if (perf->priv->max_cpu_hz == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
+static void get_cpu_cpu_threads (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->cpu_threads = loongson_dbus_call ("CpuThreads", &error);
+    if (perf->priv->cpu_threads == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
+static void get_cpu_max_mem_capacity (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->max_mem_capacity = loongson_dbus_call ("MaximumMemoryCapacity", &error);
+    if (perf->priv->max_mem_capacity == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
+static void get_cpu_memory_channel (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->memory_channel = loongson_dbus_call ("MemoryChannel", &error);
+    if (perf->priv->memory_channel == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
+static void get_cpu_max_mem_fre (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->max_mem_fre = loongson_dbus_call ("MaximumMemoryFrequency", &error);
+    if (perf->priv->max_mem_fre == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
+static void get_cpu_physical_kernel (LoongsonPerf *perf)
+{
+    g_autoptr(GError) error = NULL;
+
+    perf->priv->physical_kernel = loongson_dbus_call ("PhysicalKernel", &error);
+    if (perf->priv->physical_kernel == NULL)
+    {
+        loongson_message_dialog (_("Get loongson perf"),
+                                 WARING,
+                                 "%s", "error->message");
+    }
+}
+
 static void set_perf_data (LoongsonPerf *perf)
 {
     perf->priv->name = g_strdup (_("Loongson Perf"));
     
     get_cpu_perf_data (perf);
+    get_cpu_physical_kernel (perf);
+    get_cpu_max_mem_fre (perf);
+    get_cpu_memory_channel (perf);
+    get_cpu_max_mem_capacity (perf);
+    get_cpu_cpu_threads (perf);
+    get_cpu_max_cpu_hz (perf);
 }
 
 static void
@@ -86,7 +176,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Physical Kernel"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 0, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->physical_kernel);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 0, 1, 1);
 
@@ -95,7 +185,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Maximum Memory frequency"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 1, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->max_mem_fre);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 1, 1, 1);
 
@@ -104,7 +194,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Memory channel"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 2, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->memory_channel);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 2, 1, 1);
 
@@ -113,7 +203,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Maximum Frequency"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 3, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->max_cpu_hz);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 3, 1, 1);
 
@@ -131,7 +221,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Maximum memory capacity"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 5, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->max_mem_capacity);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 5, 1, 1);
 
@@ -140,7 +230,7 @@ loongson_perf_fill (LoongsonPerf *perf)
     set_lable_style (label, "gray", 12, _("Number of threads"), TRUE);
     gtk_grid_attach (GTK_GRID (table) ,label, 0, 6, 1, 1);
 
-    label = gtk_label_new (NULL);
+    label = gtk_label_new (perf->priv->cpu_threads);
     gtk_label_set_xalign (GTK_LABEL(label), 0);
     gtk_grid_attach (GTK_GRID (table) ,label, 1, 6, 1, 1);
 
